eb9fe35a0171cb2d53e8715577bef0bcdcd515bc
RafaQATester <orafael49@gmail.com>
2025-02-28 09:44:08 -0300

    atualização

---
+++ Modulo3/loops_aninhados.c
@@ -0,0 +1,26 @@
+/*
+    Estrutura Loop Aninhado
+
+    estrutura (condição)
+    {
+        estrutura(condiçãp)
+        {
+            //código
+        }
+    }
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    for (int i = 1; i <= 10; i++){
+
+        for (int j = 1; j <= 10; j++){
+            printf("%d\n", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
